50d7db3997f01d668357b49f7965e8a83a2dfbe1
Douglas Sanchez <douglas@eximchain.com>
2019-08-07 19:06:14 -0400

    Splash screen is now responsive.

--- src/components/froala/CallToAction/CallToAction.tsx
+++ src/components/froala/CallToAction/CallToAction.tsx
@@ -7,7 +7,7 @@ import { Link } from '@reach/router';
 
 const CallToAction = () => (
   <section className="fdb-block py-0" data-block-type="call_to_action" data-id={1}>
-    <div className="container bg-r py-5 my-5 hide-md" style={{backgroundImage: 'url(Splash.svg)'}}>
+    <div className="container bg-r py-5 my-5 hide-md" id="hero">
       <div className="row py-5">
         <div className="col-12 col-sm-10 col-md-8 col-lg-6 text-left" style={{zIndex: 10000}}>
           {/* <h1>DappBot</h1> */}
