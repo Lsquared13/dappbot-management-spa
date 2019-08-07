73631ab355a17def0caca27085f66d1cf63e3a0c
Douglas Sanchez <douglas@eximchain.com>
2019-08-07 17:50:41 -0400

    updated font and copy using third round of feedback.

--- src/components/froala/CallToAction/CallToAction.tsx
+++ src/components/froala/CallToAction/CallToAction.tsx
@@ -10,11 +10,14 @@ const CallToAction = () => (
     <div className="container bg-r py-5 my-5 hide-md" style={{backgroundImage: 'url(Splash.svg)'}}>
       <div className="row py-5">
         <div className="col-12 col-sm-10 col-md-8 col-lg-6 text-left" style={{zIndex: 10000}}>
-          <h1>DappBot</h1>
-          <h2>Tools for Blockchain Developers</h2>
-          <p className="lead">DappBot empowers blockchain learners and builders to immediately make smart contracts usable by anybody with a browser within three steps and five minutes.</p>
-          <p className="mt-4">
+          {/* <h1>DappBot</h1> */}
+          <h2>Create Dapps with only a smart contract ABI. No coding required.</h2>
+          <p className="lead">Five Minutes, Three Steps, Zero coding, build and share your Blockchain Application with a unique URL using DappBot today.</p>
+          <p className="mt-5">
             <Link className="btn btn-primary" to='/signup'>Start Your Free Trial</Link>
+          </p>
+          <p className="mt-3">
+            <a className="link" href='#apps'>Learn How it Works</a>
           </p>
         </div>
       </div>
