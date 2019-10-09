8125047ba912426e33e367ea0a95e54ed225cb27
John O'Sullivan <john@eximchain.com>
2019-10-09 19:04:40 -0400

    Figured out how to properly load global segment object via <reference>

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -1,5 +1,14 @@
 /// <reference types="react-scripts" />
 
+// Below syntax allows you to load global types without actually
+// attaching them to an import -- they're just attached to the
+// window.  Discovered this from the below GitHub issue comment:
+//
+// https://github.com/DefinitelyTyped/DefinitelyTyped/issues/17996#issuecomment-314911875
+//
+/// <reference path="../node_modules/@types/segment-analytics/index.d.ts" />
+
+
 declare module "react-floating-button-menu";
 declare module "react-copy-to-clipboard";
 declare module "*.svg" {
@@ -7,11 +16,3 @@ declare module "*.svg" {
   export default value;
 }
 declare module 'react-icons/lib/fa/close';
-// declare module '*.svg'
-
-import Analytics from  '@segment/analytics.js-core';
-export import analytics = Analytics;
-
-declare global {
-  interface Window { analytics: analytics }
-}
