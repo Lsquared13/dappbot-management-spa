055eca74a2ae67e1f81bbb603402e00a270a9bd1
John O'Sullivan <john@eximchain.com>
2019-10-17 20:09:18 -0400

    Added new client prop, dropped ' - Dapp.Bot'

--- src/services/analytics.ts
+++ src/services/analytics.ts
@@ -1,68 +1,70 @@
 import { CreateDapp } from "@eximchain/dappbot-types/spec/methods/private";
 
 const apiUrl = process.env.REACT_APP_DAPPBOT_API_ENDPOINT as string;
+const client = 'dapp.bot';
+function defaultTrackProps(){ return { apiUrl, client } }
 
 export function userLogin(email:string, isRefresh:boolean) {
   window.analytics.identify(email);
-  window.analytics.track('User Login - Dapp.Bot', {
-    email, apiUrl, isRefresh
+  window.analytics.track('User Login', {
+    email, isRefresh, ...defaultTrackProps()
   })
 }
 
 export function userSignup(email:string, metadata:Record<string,any>) {
   window.analytics.identify(email, metadata);
-  window.analytics.track('User Signup - Dapp.Bot', {
-    email, apiUrl
+  window.analytics.track('User Signup', {
+    email, ...defaultTrackProps()
   });
 }
 
 export function dappCreated(email:string, DappName:string, args:CreateDapp.Args) {
-  window.analytics.track('Dapp Created - Dapp.Bot', {
-    apiUrl, email, DappName, ...args
+  window.analytics.track('Dapp Created', {
+    email, DappName, ...args, ...defaultTrackProps()
   })
 }
 
 export function dappDeleted(DappName:string) {
-  window.analytics.track('Dapp Deleted - Dapp.Bot', {
-    apiUrl, DappName
+  window.analytics.track('Dapp Deleted', {
+    DappName, ...defaultTrackProps()
   })
 }
 
 export function cardAdded(email:string) {
   window.analytics.track('Card Added', {
-    apiUrl, email
+    email, ...defaultTrackProps()
   })
 }
 
 export function cardUpdated(email:string) {
   window.analytics.track('Card Updated', {
-    apiUrl, email
+    email, ...defaultTrackProps()
   })
 }
 
 export function capacityUpdated(email:string, oldCapacity:number, newCapacity:number) {
   if (oldCapacity > newCapacity) {
     window.analytics.track('Dapp Capacity Decreased', {
-      email, apiUrl, oldCapacity, newCapacity
+      email, oldCapacity, newCapacity, ...defaultTrackProps()
     })
   }
 
   if (newCapacity > oldCapacity) {
     window.analytics.track('Dapp Capacity Increased', {
-      email, apiUrl, oldCapacity, newCapacity
+      email, oldCapacity, newCapacity, ...defaultTrackProps()
     })
   }
 }
 
 export function appMfaEnabled(email:string) {
   window.analytics.track('MFA Enabled - App', {
-    apiUrl, email
+    email, ...defaultTrackProps()
   })
 }
 
 export function appMfaDisabled(email:string) {
   window.analytics.track('MFA Disabled - App', {
-    apiUrl, email
+    email, ...defaultTrackProps()
   })
 }
 
