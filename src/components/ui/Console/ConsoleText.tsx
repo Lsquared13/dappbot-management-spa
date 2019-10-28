0c1d7192778feeb5add70382909cf4166d30478c
Louis <loulamia@Louiss-Eximchain-MacBook-Pro.local>
2019-10-28 18:55:02 -0400

    Clean up console text

--- src/components/ui/Console/ConsoleText.tsx
+++ src/components/ui/Console/ConsoleText.tsx
@@ -1,33 +1,33 @@
 export const ConsoleText = {
   line1 : "$ dappsmith create --dapp",
 
-  line2 : `this tool will parse your ABI and create and host a dapp at the specified
-  subdomain. The process takes around 5 minutes to build and deploy. Happy
+  line2 : `This tool will parse your ABI and create and host a dapp on 165 hosts around
+  the world. The process takes 5 minutes or less to build and deploy. Happy
   hacking!`,
 
-  line3a : "[+] Fetching Dapp Seed",
-  line3b : "[+] Loading ...",
+  line3a : "[+] Fetching dapp seed",
+  line3b : "[+] Loading...",
   line3c : "[+] Complete, cleaning up resources...",
   line3d : "[+] Ready, to build some dapps...",
 
-  line4heading : "> Building Dapp this will take some time, (DO NOT REFRESH THE PAGE)",
-  line4a : "[+] Fetching Pythons ...",
-  line4b : "[+] Calling Snake Charmers ...",
+  line4heading : "> Building dapp, this will take some time (DO NOT REFRESH THE PAGE)",
+  line4a : "[+] Fetching Pythons...",
+  line4b : "[+] Calling snake charmers...",
   line4c : "[+] Playing a tune...",
   
   line5heading : "> CodeGen in progress (DO NOT REFRESH THE PAGE)",
-  line5 : "[+] [2/519] Generating Models ...",
+  line5 : "[+] [2/519] Generating Models...",
 
-  line6 : "[+] [30/519] Generating Views & Controllers ...",
+  line6 : "[+] [30/519] Generating Views & Controllers...",
 
   line7heading : "Inject blockchain capabilities",
-  line7a : "[+] [156/519] Injecting web3 ...",
+  line7a : "[+] [156/519] Injecting web3...",
   line7b : "[+] [168/519] Injecting metamask...",
   line7c : "[+] [232/519] Setting up smart contract...",
   line7d : "[+] [232/519] Generate a component for each smart contract method...",
 
   line8heading : "Configure Web Infrastructure",
-  line8a : "[+] [416/519] Reserving Subdomain ...",
+  line8a : "[+] [416/519] Reserving subdomain...",
   line8b : "[+] [487/519] Configuring CNAME record...",
   line8c : "[+] [503/519] Setting up https...",
   line8d : "[+] [518/519] Propagating DNS record [ T - 3 min ]...",
